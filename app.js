2b92c5e0493a7cdf778ba2dc000674749fa1d244
John Brock <_johnbrock@outlook.com>
2014-05-29 22:47:50 -0700

    WebCheck v0.01

---
+++ app.js
@@ -0,0 +1,132 @@
+// app.js
+var express = require('express');
+var logfmt = require('logfmt');
+var nodemailer = require('nodemailer');
+var util = require('util');
+var dateFormat = require('dateformat');
+var request = require('request');
+var config = require('./config.json');
+
+var app = express();
+app.use(logfmt.requestLogger());
+
+var intervalInSeconds = config['interval_in_seconds'];
+var timeoutInSeconds = config['timeout_in_seconds'];
+var endpoint = config['endpoint'];
+var endpointTitle = config['endpoint_title'];
+var checkSsl = config['check_ssl'];
+var bodyText = config['body_text_to_check'];
+var emailsToNotify = config['emails_to_notify_on_error'];
+var emailService = config['email_service'];
+var username = config['username'];
+var password = config['password'];
+
+var monitorOn = false;
+var timerId;
+
+var mailer = nodemailer.createTransport('SMTP', {
+	service: emailService,
+	auth: {
+		user: username,
+		pass: password
+	}
+});
+
+function monitor() {
+	timerId = setInterval(function() {
+		checkEndpoint(endpoint, function(err) {
+			if(null != err && err.length > 0) {
+				logfmt.log({'endpoint_error' : err});
+				notifyRecipientsOfError(err, getDateNow());
+			}
+		});
+	}, intervalInSeconds*1000);
+}
+
+function checkEndpoint(endpoint, callback) {
+	request({ uri: endpoint, strictSSL: checkSsl, timeout: timeoutInSeconds*1000 }, function(err, resp, body) {
+		var error;
+		if (err) {
+			var baseError = 'Request timed out';
+			error = checkSsl ? baseError + ' or SSL was expired / invalid' : baseError;
+		} else if (resp.statusCode != 200) {
+			error = 'Status code was not expected: ' + resp.statusCode;
+		} else if (null != bodyText && bodyText.length > 0) {
+			if (JSON.stringify(body).indexOf(bodyText) == -1) {
+				error = 'Expected body text not found in response';
+			}
+		}
+		callback(error);
+	});
+}
+
+function notifyRecipientsOfError(error, dateTime) {
+	var subject = util.format('[URGENT] %s needs attention', endpointTitle);
+	var bodyText = util.format('%s did not respond successfully to WebCheck. Error: %s. %s', endpoint, error, dateTime);
+	sendEmail(subject, bodyText);
+}
+
+function notifyOfMonitoringStatusChange(turnedOn, dateTime) {
+	var subject = util.format('[INFO] %s monitor turned %s', endpointTitle, turnedOn ? 'ON' : 'OFF');
+	var bodyText = util.format('Please ensure this is the desired WebCheck state. Change made on %s.', dateTime);
+	sendEmail(subject, bodyText);
+}
+
+function sendEmail(subject, bodyText) {
+	var mailOptions = {
+		from: util.format('WebCheck <%s>', username),
+		to: emailsToNotify.join(','),
+		subject: subject,
+		text: bodyText,
+		html: '<b>' + bodyText + '<b>'
+	};
+	mailer.sendMail(mailOptions, function(err, resp) {
+		if(null != err) {
+			logfmt.log({'email_error': JSON.stringify(err)});
+		} else {
+			logfmt.log({'email_success': resp.message});
+		}
+	})
+}
+
+function toggleMonitoringService(turnOn, res) {
+	var msgToSend;
+	var baseMsg = 'Monitoring service ';
+	if (monitorOn == turnOn) {
+		msgToSend = baseMsg + util.format('already turned %s.', monitorOn ? 'on' : 'off');
+	} else {
+		if (turnOn) {
+			monitor();
+		} else {
+			clearInterval(timerId);
+		}
+		monitorOn = turnOn;
+		msgToSend = baseMsg + util.format('turned %s!', turnOn ? 'ON' : 'OFF');
+		notifyOfMonitoringStatusChange(turnOn, getDateNow());
+	}
+	res.send(msgToSend);
+}
+
+function getDateNow() {
+	return dateFormat(Date.now(), "dddd, mmmm dS, yyyy, h:MM:ss TT");
+}
+
+app.get('/', function(req, res) {
+	var baseMessage = util.format('The monitoring service for %s is currently %s.', endpoint, monitorOn ? 'ON' : 'OFF');
+	var instructions = '\n\n/start will turn monitoring on\n/stop will turn monitoring off';
+	res.setHeader('content-type', 'text/plain');
+	res.send(baseMessage + instructions);
+});
+
+app.get('/start', function(req, res) {
+	toggleMonitoringService(true, res);
+});
+
+app.get('/stop', function(req, res) {
+	toggleMonitoringService(false, res);
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+	console.log('Monitoring service server started on port: ' + port);
+});
